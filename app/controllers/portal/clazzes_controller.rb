77b6445b5a0a350d860afdccb0166869faaa2db8
Sagar Ghadge <sagar.ghadge@zeuslearning.com>
2012-07-25 12:40:14 +0530

    Create Class Full Status page
    - Create Class Full Status page which show all
      students progress across all the offerings
    
    - View progress of students in an offering activity wise

--- app/controllers/portal/clazzes_controller.rb
+++ app/controllers/portal/clazzes_controller.rb
@@ -630,4 +630,12 @@ class Portal::ClazzesController < ApplicationController
     
   end
   
+  def fullstatus
+    if current_user.anonymous?
+      redirect_to home_url
+      return
+    end
+    @portal_clazz = Portal::Clazz.find(params[:id]);
+  end
+  
 end
