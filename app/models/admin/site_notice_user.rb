d9d5f673bf2902ce963c95f5b3e465db67104f2b
Sunny Singh <sunny.singh@zeuslearning.com>
2012-07-31 15:51:06 +0530

    Optimize site notices functionality
    - Optimize notice create
    
    - Optimize notice update
    
    - Optimize notice display

--- app/models/admin/site_notice_user.rb
+++ app/models/admin/site_notice_user.rb
@@ -1,10 +1,10 @@
 
 D:\GIT\RIGSE>@git.exe %*
-class Admin::SiteNoticeRole < ActiveRecord::Base
+class Admin::SiteNoticeUser < ActiveRecord::Base
   belongs_to :site_notice, :class_name => 'Admin::SiteNotice', :foreign_key => 'notice_id', :primary_key => 'id'
-  belongs_to :role, :class_name => "Role", :foreign_key => "role_id"
+  belongs_to :user, :class_name => "User", :foreign_key => "user_id"
   
-  validates :notice_id, :role_id, :presence => true
+  validates :notice_id, :user_id, :presence => true
 end
 
 D:\GIT\RIGSE>@set ErrorLevel=%ErrorLevel%
