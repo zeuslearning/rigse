11b80eae903a3af943a55cbf946029083820a0ff
Nitesh Patil <nitesh.patil@zeuslearning.com>
2012-11-03 12:56:01 +0530

    Modify Share functionality
    - Modify Share functionality in material preview page
    
    - Implement expand-collapse effect for share link

---
+++ app/assets/javascripts/share_material.js
@@ -0,0 +1,35 @@
+var expandedShareButtonid;
+function showShareoptions(id){
+    if (animating)
+    {
+        return false;
+    }
+    if(id!=expandedShareButtonid)
+    {
+        $$(".sharing").each(function(shareContainer){shareContainer.hide();shareContainer.removeClassName('visible');});
+        $$(".share_Button").each(function(sharebtn){sharebtn.update("Share &#9660");});
+    }
+    var shareContainer=$("share_"+id);
+    var afterFinishCallback = function(){
+        animating = false;
+        $('shareExpandCollapse_'+id).update(expandCollapseText);
+    };
+    
+    if (shareContainer.hasClassName('visible'))
+    {
+        Effect.BlindUp(shareContainer, { duration: 0.2, afterFinish: afterFinishCallback });
+        shareContainer.removeClassName('visible');
+        expandCollapseText = "Share &#9660;";
+        animating = true;
+    }
+    else
+    {
+        Effect.BlindDown(shareContainer, { duration: 0.2, afterFinish: afterFinishCallback });
+        shareContainer.addClassName('visible');
+        expandCollapseText = "Share &#9650;";
+        expandedShareButtonid=id;
+        animating = true;
+    }
+    return true;
+}
+
