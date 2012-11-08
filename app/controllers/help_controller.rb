736aa475632c29de628744e2930db8c4a2ae22b4
Sunny Singh <sunny.singh@zeuslearning.com>
2012-11-08 18:09:38 +0530

    Refactor help and home page code.
    - Use switch statements in place of if else if else if statements.

--- app/controllers/help_controller.rb
+++ app/controllers/help_controller.rb
@@ -2,8 +2,9 @@ class HelpController < ApplicationController
   caches_page   :project_css
   theme "rites"
   
-  def index
-    case current_project.help_type
+  
+  def get_help_page(help_type)
+    case help_type
     when 'no help'
       render :template => "help/no_help_page"
     when 'external url'
@@ -14,24 +15,20 @@ class HelpController < ApplicationController
     end
   end
   
+  def index
+    help_type = current_project.help_type
+    get_help_page(help_type)
+  end
+  
   def preview_help_page
     if (params[:preview_help_page_from_edit])
       response.headers["X-XSS-Protection"] = "0"
-      @help_page_preview_content = params[:preview_help_page_from_edit]
+      @help_page_content = params[:preview_help_page_from_edit]
       return
     end
-
     @preview_help_project_id = params[:preview_help_page_from_summary_page] || @preview_help_project_id
     @preview_help_project_id = @preview_help_project_id.to_i
     preview_project = Admin::Project.find_by_id(@preview_help_project_id)
-    case preview_project.help_type
-    when 'no help'
-      render :template => "help/no_help_page"
-    when 'external url'
-      external_url = preview_project.external_url
-      redirect_to "#{external_url}"
-    when 'help custom html'
-      @help_page_preview_content = preview_project.custom_help_page_html
-    end
+    get_help_page(preview_project.help_type)
   end
 end
